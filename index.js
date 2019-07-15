db87578b1e3a8e53ac2df38c0fcdd3e01739d8f9
ZeroPandas <xxdarkbluedevilxx@gmail.com>
2019-07-15 18:35:54 +0300

    Additional ui design added to the num pad.

--- index.js
+++ index.js
@@ -6,17 +6,35 @@
     const keypadElement = document.getElementById('keypad');
     keypadElement.addEventListener('click', function () {
         bigListElement.style.marginLeft = '0';
+        keypadElement.style.color = '#1abc9c';
+
+        if (keypadElement.style.color = '#1abc9c') {
+            resentsElement.style.color = '#f5f5f5';
+            contactsElement.style.color = '#f5f5f5';
+        }
     });
 
     const resentsElement = document.getElementById('resents');
     resentsElement.addEventListener('click', function () {
         bigListElement.style.marginLeft = '-405px';
+        resentsElement.style.color = '#1abc9c';
+
+        if (resentsElement.style.color = '#1abc9c') {
+            keypadElement.style.color = '#f5f5f5';
+            contactsElement.style.color = '#f5f5f5';
+        }
     });
 
 
     const contactsElement = document.getElementById('contacts');
     contactsElement.addEventListener('click', function () {
         bigListElement.style.marginLeft = '-810px';
+        contactsElement.style.color = '#1abc9c';
+
+        if (contactsElement.style.color = '#1abc9c') {
+            keypadElement.style.color = '#f5f5f5';
+            resentsElement.style.color = '#f5f5f5';
+        }
     });
 
 })();
